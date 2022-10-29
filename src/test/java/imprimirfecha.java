d38c567688579dfe4a99f746488ecf1395966c89
hernan35 <locoroal31@gmail.com>
2022-10-28 22:56:44 -0500

    Repos Fenix

---
+++ src/test/java/imprimirfecha.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class imprimirfecha {
+    String Fecha = "26/01/1985";
+
+    @Test
+    public void Date(){
+        System.out.printf("La Fecha de Nacimiento es: \n"+ Fecha +"\n");
+    }
+}
